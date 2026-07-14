2b59ae10d14d2af78f751dd649ce15333eac2b79
Jayaharish Ravichandran <168050215+JayaharishMR@users.noreply.github.com>
2026-07-14 21:56:59 +0530

    Time: 0 ms (100%), Space: 41.9 MB (85.35%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,6 +2,18 @@ class Solution {
     public int fib(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
-        return fib(n-1)+fib(n-2);
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        return fib(n, arr);
+    }
+
+    public int fib(int n, int[] arr) {
+        if (n == 0) return 0;
+        if (arr[n] != 0) {
+            return arr[n];
+        }
+        arr[n] = fib(n-1, arr)+fib(n-2,arr);
+        return arr[n];
     }
 }
