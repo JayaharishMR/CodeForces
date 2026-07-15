d06198b32aabdb916ff90e9e72e3342ecd3f4e98
Jayaharish Ravichandran <168050215+JayaharishMR@users.noreply.github.com>
2026-07-15 23:17:24 +0530

    Time: 1 ms (80.56%), Space: 43.3 MB (23.12%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,30 +1,23 @@
 class Solution {
     public int numDecodings(String s) {
-        Set<String> validArr = new HashSet<>();
-        for(int i = 1; i <= 26; i++) {
-            validArr.add(String.valueOf(i));
-        }
         int n = s.length();
         int[] dp = new int[n+1];
         dp[n] = 1;
-        dp[n-1] = validArr.contains(String.valueOf(s.charAt(n-1))) ? 1 : 0;
+        dp[n-1] = !String.valueOf(s.charAt(n-1)).equals("0") ? 1 : 0;
         for (int i = n-2; i >=0; i--) {
             String a = String.valueOf(s.charAt(i));
-            String b = String.valueOf(s.charAt(i+1));
             if (a.equals("0")){
                 dp[i] = 0;
                 continue;
             }
+            int num = (s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0');
              //choice 2
-            if (validArr.contains(a+b)) {
+            if (num >= 10 && num <= 26) {
                 dp[i] = dp[i+1] + dp[i+2];
                 continue;
             }
             //choice 1
-            if (validArr.contains(a)) {
-                dp[i] = dp[i+1];
-            }
-
+            dp[i] = dp[i+1];
            
         }
         return dp[0];
