7b23b08f12380bc1d9ceb1708f82afbf505e9d50
Jayaharish Ravichandran <168050215+JayaharishMR@users.noreply.github.com>
2026-07-21 00:05:15 +0530

    Time: 2 ms (19.55%), Space: 61.4 MB (29.68%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,43 +1,22 @@
 class Solution {
+    void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k %= n;
+        int length = nums.length;
+        k %= length;
         if (k == 0) return;
 
-        rotate(nums, 0, n, k);
-    }
-
-    private void rotate(int[] nums, int start, int len, int k) {
-        if (k == 0 || k == len) return;
-
-        int left = len - k;
-        int right = k;
-
-        if (left == right) {
-            swap(nums, start, start + left, left);
-            return;
-        }
-
-        if (left > right) {
-            // Swap last 'right' elements of left block
-            swap(nums, start + left - right, start + left, right);
-
-            // Solve remaining left block
-            rotate(nums, start, left, k);
-        } else {
-            // Swap first 'left' elements of right block
-            swap(nums, start, start + left, left);
-
-            // Solve remaining right block
-            rotate(nums, start + left, right, k - left);
-        }
-    }
-
-    private void swap(int[] nums, int i, int j, int len) {
-        for (int x = 0; x < len; x++) {
-            int temp = nums[i + x];
-            nums[i + x] = nums[j + x];
-            nums[j + x] = temp;
-        }
+        int someMid = length - k;
+        reverse(nums, 0, someMid - 1);
+        reverse(nums, someMid, length - 1);
+        reverse(nums, 0, length - 1);
     }
 }
