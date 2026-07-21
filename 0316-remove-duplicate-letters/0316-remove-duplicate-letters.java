5b5e86923fd68d0f352fd76a8a6b9954e389a279
Jayaharish Ravichandran <168050215+JayaharishMR@users.noreply.github.com>
2026-07-21 22:32:41 +0530

    Time: 2 ms (85.71%), Space: 43.7 MB (68.23%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -8,20 +8,17 @@ class Solution {
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < s.length(); i++) {
             char curr = s.charAt(i);
-            System.out.println(curr+" vis: "+vis[curr-'a']);
             if (vis[curr-'a'] == true) {
                 arr[curr -'a']--;
                 continue;
             }
             while (sb.length() >= 1 && sb.charAt(sb.length()-1) > curr && arr[sb.charAt(sb.length()-1) - 'a'] != 0) {
-                System.out.println("deleting "+sb.charAt(sb.length()-1));
                 vis[sb.charAt(sb.length()-1) - 'a'] = false;
                 sb.deleteCharAt(sb.length()-1);
             }
             sb.append(curr);
             vis[curr-'a'] = true;
             arr[curr -'a']--;
-            System.out.println(sb.toString());
         }
         return sb.toString();
     }
