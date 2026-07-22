580736e3b9fd7e060161ed9b7851801277ed76aa
Jayaharish Ravichandran <168050215+JayaharishMR@users.noreply.github.com>
2026-07-22 21:14:52 +0530

    Time: 3 ms (18.86%), Space: 47.8 MB (57.2%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,28 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int l =0;
-        int r =0;
-        if (nums.length <= 1) {
-            return;
-        }
-        while (r < nums.length) {
-            if (nums[l] == 0) {
-                while (r <  nums.length && nums[r] == 0) {
-                    r++;
-                }
-                swap(nums, l, r);
-                l++;
+        //saw a solution called snowball and it is good
+        int snowBall = 0;
+        for (int i  = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                snowBall++;
             } else {
-                l++;
-                r++;
+                if (snowBall  == 0) continue;
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[i - snowBall] = temp; 
             }
         }
-    }
-
-    public void swap(int[] nums, int l, int r) {
-        if (r >= nums.length) return;
-        int temp = nums[l];
-        nums[l] = nums[r];
-        nums[r] = temp;
     }
 }
