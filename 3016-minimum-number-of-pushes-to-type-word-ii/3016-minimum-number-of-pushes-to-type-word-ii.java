e46fc5977bc5dfd5e5915537cc8a4aa8df7548f2
Jayaharish Ravichandran <168050215+JayaharishMR@users.noreply.github.com>
2026-07-31 22:17:45 +0530

    Time: 207 ms (5.49%), Space: 53.7 MB (17.34%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimumPushes(String word) {
+        Map<String, Integer> hm = new HashMap<>();
+        if (word.length() <= 8) {
+            return word.length();
+        }
+        for (int i = 0; i < word.length(); i++) {
+            hm.put(String.valueOf(word.charAt(i)), hm.getOrDefault(String.valueOf(word.charAt(i)),0)+1);
+        }
+
+        PriorityQueue<Map.Entry<String,Integer>> pq = new PriorityQueue<>(
+            Map.Entry.<String, Integer>comparingByValue().reversed()
+        );
+        pq.addAll(hm.entrySet());
+        int count = 0;
+        int distinct = 0;
+        while (!pq.isEmpty()) {
+            Map.Entry<String, Integer> entry = pq.poll();
+            int value = entry.getValue();
+            System.out.println("count: "+count+"key: "+entry.getKey()+"value: "+entry.getValue());
+            for (int i  = 0; i < value; i++) {
+                count = count + 1+ distinct/8;
+            }
+            distinct++;
+        }
+        return count;
+    }
+}
