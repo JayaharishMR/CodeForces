d22801396d79e4faccbd0343dc56d21489e995ae
Jayaharish Ravichandran <168050215+JayaharishMR@users.noreply.github.com>
2026-09-08 21:16:36 +0530

    Time: 1 ms (83.65%), Space: 42.5 MB (72.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -3,21 +3,6 @@ class Solution {
         if (n < 1000) {
             return 0;
         }
-        int res = 0;
-        int m = 4;
-        int checkPoint = 10000;
-        for (int i = 1000; i <= n; i++) {
-            int a = m/3;
-            if (m%3 == 0) {
-                a--;
-            }
-            res += a;
-            if (i == checkPoint) {
-                m++;
-                checkPoint *= 10;
-            }
-        }
-        return res;
+        return (n-1000)+1;
     }
-
 }
